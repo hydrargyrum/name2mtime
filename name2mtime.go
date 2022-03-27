57dfca632df9772503bd85508638f87473a784a1
Hg <dev@indigo.re>
2022-03-27 21:52:14 +0200

    minor cleanup by removing obsolete timestamp detection

--- name2mtime.go
+++ name2mtime.go
@@ -8,31 +8,12 @@ import "regexp"
 import "strconv"
 import "time"
 
-// 2006-01-02T15:04:05.999999999Z07:00
-
-var formats = []string{
-	"2006-01-02T15:04:05",
-	"20060102_150405",
-	"IMG_20060102_150405",
-	"VID_20060102_150405",
-	"VID_20060102_150405",
-}
-
 var formatsRePatterns = []string{
 	"(\\d{4})-(\\d{2})-(\\d{2})[T_](\\d{2}):(\\d{2}):(\\d{2})",
 	"(\\d{4})-(\\d{2})-(\\d{2})-(\\d{2})-(\\d{2})-(\\d{2})",
 	"(\\d{4})(\\d{2})(\\d{2})[_ ](\\d{2})(\\d{2})(\\d{2})",
 	"(\\d{4})-(\\d{2})-(\\d{2})",
 	"(\\d{4})(\\d{2})(\\d{2})",
-}
-
-func tryParse(filename string) (*time.Time, error) {
-	for _, format := range formats {
-		if parsed, err := time.Parse(format, filename); err == nil {
-			return &parsed, nil
-		}
-	}
-	return nil, errors.New("unrecognized format")
 }
 
 func parseIntArray(strs []string) []int {
@@ -43,7 +24,7 @@ func parseIntArray(strs []string) []int {
 	return ret
 }
 
-func tryParse2(filename string) (*time.Time, error) {
+func tryParse(filename string) (*time.Time, error) {
 	// XXX can't have this as a global field?
 	formatsRe := make([]*regexp.Regexp, len(formatsRePatterns))
 	for i, pattern := range formatsRePatterns {
@@ -71,7 +52,7 @@ func main() {
 	for _, filepath := range os.Args[1:] {
 		filename := path.Base(filepath)
 
-		if parsed, err := tryParse2(filename); err == nil {
+		if parsed, err := tryParse(filename); err == nil {
 			if err := os.Chtimes(filepath, *parsed, *parsed); err != nil {
 				log.Printf("could not change times of %q: %s\n", filepath, err)
 			} else {
